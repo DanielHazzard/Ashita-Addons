1b13f3a29ca6d50b54dc213954b232b76cd2e603
Daniel Hazzard <20212218+DanielHazzard@users.noreply.github.com>
2018-03-08 17:06:30 +0000

    Set weaponskill back to none

--- UnityNMFighter/UnityNMFighter.lua
+++ UnityNMFighter/UnityNMFighter.lua
@@ -10,7 +10,7 @@ modification, are permitted provided that the following conditions are met:
     * Redistributions in binary form must reproduce the above copyright
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
-    * Neither the name of <addon name> nor the
+    * Neither the name of UnityNMFighter nor the
       names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
 
@@ -41,7 +41,7 @@ variables = {}
 variables.WarpMethod = 0
 
 -- The weaponskill to use, Example: Chant du Cygne
-variables.weaponskill = 'Savage Blade'
+variables.weaponskill = 'None'
 
 -- 1 = Attack, 2 = Pet Fight
 variables.EngageType = 1 
